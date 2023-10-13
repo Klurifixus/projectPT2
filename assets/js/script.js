0d553e469c514cf5c646e71dd04ef965040880e5
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-10-13 12:20:11 +0200

    add cycle to html,css+ function to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,28 @@ let breakMin = document.getElementById('break-length');
 
 let minutes = document.getElementById('minutes');
 let seconds = document.getElementById('seconds');
+let bminutes = document.getElementById('bminutes');
+let bseconds = document.getElementById('bseconds');
+
+//timer variable reference
+let startTimer;
+
+function timer(){
+    if(seconds.innerText != 0){
+        seconds.innerText --;
+    } else if(minutes.innerText != 0 && seconds.innertext ==0){
+        seconds.innertext = 59;
+        minutes.innertext--;
+    }
+
+    if(minutes.innerText == 0 && seconds.innerText == 0){
+        if(bseconds.innerText != 0){
+            bseconds.innerText --;
+        } else if(bminutes.innerText != 0 && bseconds.innertext ==0){
+            bseconds.innertext = 59;
+            bminutes.innertext--;
+        }
+    }    
+}
+
 
