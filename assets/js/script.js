81ffa27ac6408e95860520357244775db9342d10
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-10-13 14:49:40 +0200

    add eventlistner for input in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
-
 let startBtn = document.getElementById('start-btn');
 let stopBtn = document.getElementById('stop-btn');
 let resetBtn = document.getElementById('reset-btn');
-
 
 let workMin = document.getElementById('work-length');
 let breakMin = document.getElementById('break-length');
@@ -15,44 +13,35 @@ let bseconds = document.getElementById('bseconds');
 //timer variable reference
 let startTimer;
 
-
-
 startBtn.addEventListener('click', function(){
     if(startTimer === undefined){
         startTimer = setInterval(timer, 1000);
-    } else{
-      alert("Timer is already running");
+    } else {
+        alert("Timer is already running");
     }
-
-
 });
 
-
-
 stopBtn.addEventListener('click', function(){
-    if(stopTimer === undefined){
-        stopInterval(); 
-        
-    }
+    stopInterval();
     startTimer = undefined;
-})
-resetBtn.addEventListener('click', function (){
-    minutes.innerText = 25;
+});
+
+resetBtn.addEventListener('click', function() {
+    minutes.innerText = workMin.value;
     seconds.innerText = "00";
 
-    bminutes.innerText = 5;
+    bminutes.innerText = breakMin.value;
     bseconds.innerText = "00";
-    document.getElementById('cycles').innerText =0;{}
+    document.getElementById('cycles').innerText = 0;
+    stopInterval();
     startTimer = undefined;
-
-})
-
+});
 
 function timer(){
     //work timer
     if(seconds.innerText != 0){
-        seconds.innerText --;
-    } else if(minutes.innerText != 0 && seconds.innerText ==0){
+        seconds.innerText--;
+    } else if(minutes.innerText != 0 && seconds.innerText == 0){
         seconds.innerText = 59;
         minutes.innerText--;
     }
@@ -60,27 +49,35 @@ function timer(){
     //break timer
     if(minutes.innerText == 0 && seconds.innerText == 0){
         if(bseconds.innerText != 0){
-            bseconds.innerText --;
-        } else if(bminutes.innerText != 0 && bseconds.innerText ==0){
+            bseconds.innerText--;
+        } else if(bminutes.innerText != 0 && bseconds.innerText == 0){
             bseconds.innerText = 59;
             bminutes.innerText--;
         }
     }  
     
-    //cycletimer
-    if(minutes.innerText == 0 && seconds.innerText == bminutes.innerText == 0 && bseconds.innerText == 0){
-        minutes.innerText == 25;
-        seconds.innerText == "00";
-
-        bminutes.innerText == 5;
-        bseconds.innerText == "00";
-
+    //cycle timer
+    if(minutes.innerText == 0 && seconds.innerText == 0 && bminutes.innerText == 0 && bseconds.innerText == 0){
+        minutes.innerText = workMin.value;
+        seconds.innerText = "00";
+        bminutes.innerText = breakMin.value;
+        bseconds.innerText = "00";
         document.getElementById('cycles').innerText++;
     }
 }
-
 
 //stop timer
 function stopInterval(){
     clearInterval(startTimer);
 }
+
+// event listeners input 
+workMin.addEventListener('input', function() {
+    minutes.innerText = workMin.value;
+    seconds.innerText = "00";
+});
+
+breakMin.addEventListener('input', function() {
+    bminutes.innerText = breakMin.value;
+    bseconds.innerText = "00";
+});
