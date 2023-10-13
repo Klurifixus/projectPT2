650211c9a8debf084550be682981dcefe972be91
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-10-13 11:17:52 +0200

    js work by src in html+ getelementbyid to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
+let startBtn = document.getElementById('start-btn');
+let stopBtn = document.getElementById('stop-btn');
+let resetBtn = document.getElementById('reset-btn');
+
+
+let workMin = document.getElementById('work-length');
+let breakMin = document.getElementById('break-length');
 
