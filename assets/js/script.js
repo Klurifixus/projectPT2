77cb38048e9adfa6ebd6cb06e32bfdded81cc7eb
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-10-18 20:15:41 +0200

    added all const and 1 let to connect with html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,28 @@
+//Elements
+const trainingToggle = document.getElementById('training-toggle');
+const exerciseInput = document.getElementById('exercise-input');
+const repsInput = document.getElementById('reps-input');
+const addExerciseBtn = document.getElementById('add-exercise-btn');
+const startBtn = document.getElementById('start-btn');
+const stopBtn = document.getElementById('stop-btn');
+const resetBtn = document.getElementById('reset-btn');
+const muteBtn = document.getElementById('mute-btn');
+const timer = document.querySelector('.timer');
+const breakTimer = document.querySelector('.break-timer');
+const exerciseList = document.getElementById('exercise-list');
+const trainingSection = document.getElementById('training-section');
+const timerSection = document.getElementById('timer-section');
+
+//states
 let workTimer = 25 * 60;
 let breakTimer = 5 * 60;
 let isTrainingmode = false;
 let currentExerciseIndex = 0;
+let remainingExerciseTime = 0;
 let exercises = [];
 let isSoundMuted = true;
 
+//Audio
 const sound = new Audio('assets/sounds/battle_horn_1-6931.mp3');
 
 function toggleTrainingMode() {
@@ -25,5 +43,5 @@ function resetTimer(){
 
 }
 function toggleSound(){
-    
+
 }
