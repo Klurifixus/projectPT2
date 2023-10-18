98ebdbf57574c6d9815f2d3c2a5af5d5161bf0a6
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-10-18 19:58:13 +0200

    set up basics

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+let workTimer = 25 * 60;
+let breakTimer = 5 * 60;
+let isTrainingmode = false;
+let currentExerciseIndex = 0;
+let exercises = [];
+let isSoundMuted = true;
+
+const sound = new Audio('assets/sounds/battle_horn_1-6931.mp3');
+
+function toggleTrainingMode() {
+    isTraningmode = !isTrainingmode;
+
+}
+function addExercise(exercise, reps) {
+    exercices.push({exercise, reps});
+}
+
+function startTimer(){
+
+}
+function stopTimer(){
+
+}
+function resetTimer(){
+
+}
+function toggleSound(){
+    
+}
