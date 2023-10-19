49bbd05b89eafbb8753f01ba6d975809ea017e58
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-10-20 01:07:23 +0200

    deleted unwanted button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,50 @@
-let start = getElementById('start-btn');
-let pause = getElementById('pause-btn');
-let reset = getElementById('reset-btn');
-let stop = getElementById('stop-btn');
-let mute = getElementById('mute-btn');
+let start = document.getElementById('start-btn');
+let pause = document.getElementById('stop-btn');
+let reset = document.getElementById('reset-btn');
+let mute = document.getElementById('mute-btn');
 
-let minutes = getElementById('minutes');
-let seconds = getElementById('seconds');
-let bminutes = getElementById('bminutes');
-let bseconds = getElementById('bseconds');
+let minutes = document.getElementById('minutes');
+let seconds = document.getElementById('seconds');
+let bminutes = document.getElementById('bminutes');
+let bseconds = document.getElementById('bseconds');
 
-// refernce to timer variabel
+// Reference to timer variable
 let startTimer;
-start.addEventListner('click', function(){
-    if (startTimer === undefined){
-    startTimer = setInterval(timer, 1000)
-}
 
-})
-
-function timer(){
-    //worktimer
-    if (seconds.innerText != 0){
-        seconds.innerText--;
-    } else if(minutes.innerText != 0 && seconds.innerText ==0){
-        seconds.innerText = 59;
-        minutes.innerText--;
-    } //breaktimer
-    if (bseconds.innerText != 0){
-        bseconds.innerText--;
-    } else if(bminutes.innerText != 0 && bseconds.innerText ==0){
-        bseconds.innerText = 59;
-        bminutes.innerText--;
+start.addEventListener('click', function () {
+    if (startTimer === undefined) {
+        startTimer = setInterval(timer, 1000);
+    } else {
+        alert("Timer is already running.");
     }
-    if (minutes.innerText == 0 && seconds.innerText == 0 && bminutes.innerText == 0 && bseconds == 0){
-        minutes.innerText == 25;
-        seconds.innerText == "00";
-        bminutes.innerText == 5;
-        bseconds.innerText == "00";
+});
+
+function timer() {
+    // Work timer
+    if (minutes.innerText != 0 || seconds.innerText != 0) {
+        if (seconds.innerText == 0) {
+            seconds.innerText = 59;
+            minutes.innerText--;
+        } else {
+            seconds.innerText--;
+        }
+    } else if (bminutes.innerText != 0 || bseconds.innerText != 0) {
+        // Break timer
+        if (bseconds.innerText == 0) {
+            bseconds.innerText = 59;
+            bminutes.innerText--;
+        } else {
+            bseconds.innerText--;
+        }
+    }
+
+    // When both timers reach 0
+    if (minutes.innerText == 0 && seconds.innerText == 0 && bminutes.innerText == 0 && bseconds.innerText == 0) {
+        minutes.innerText = "25";
+        seconds.innerText = "00";
+        bminutes.innerText = "5";
+        bseconds.innerText = "00";
 
         document.getElementById('counter').innerText++;
-
-
     }
 }
