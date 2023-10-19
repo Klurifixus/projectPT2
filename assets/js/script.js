9bb2e22d62c8b4476de449ca39048c09acde60d9
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-10-20 00:10:26 +0200

    set first element by id

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let start = getElementById('start-btn');
+let pause = getElementById('pause-btn');
+let reset = getElementById('reset-btn');
+let stop = getElementById('stop-btn');
+let mute = getElementById('mute-btn');
+
+let minutes = getElementById('minutes');
+let seconds = getElementById('seconds');
+let bminutes = getElementById('bminutes');
+let bseconds = getElementById('bseconds');
